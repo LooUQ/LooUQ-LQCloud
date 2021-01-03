cb38aa4afc5aa4136021adea7b782a4b77ff44d6
Greg Terrell <GregTerrell@users.noreply.github.com>
2021-01-03 08:18:14 -0500

    added comments to examples dvcsettings.h

--- tests/PlatformTest/dvcSettings.h
+++ tests/PlatformTest/dvcSettings.h
@@ -5,28 +5,23 @@
 #ifndef __DVCSETTINS_H__
 #define __DVCSETTINS_H__
 
-#include <lqCloud.h>
 
 
-/* Example SAS tokens
-HostName=iothub-dev-pelogical.azure-devices.net;DeviceId=e8fdd7df-2ca2-4b64-95bc-031c6b199299;SharedAccessSignature=SharedAccessSignature sr=iothub-dev-pelogical.azure-devices.net%2Fdevices%2Fe8fdd7df-2ca2-4b64-95bc-031c6b199299&sig=ZGr8hjqBMcFHmMDc%2Bgjo3gFAaoiRazEnLTMBtsDWNpI%3D&se=1607176494
-HostName=iothub-dev-pelogical.azure-devices.net;DeviceId=e8fdd7df-2ca2-4b64-95bc-031c6b199299;SharedAccessSignature=SharedAccessSignature sr=iothub-dev-pelogical.azure-devices.net%2Fdevices%2Fe8fdd7df-2ca2-4b64-95bc-031c6b199299&sig=mRksB7BM%2Bmya6P3fkoeHFVnSL%2FHSNrTIZAL8AEZ6gJA%3D&se=1610056256
+/* Example Connection Strings >>> SAS tokens
+// Obtain from LQCloud.Manage|Devices
 HostName=iothub-dev-pelogical.azure-devices.net;DeviceId=e8fdd7df-2ca2-4b64-95bc-031c6b199299;SharedAccessSignature=SharedAccessSignature sr=iothub-dev-pelogical.azure-devices.net%2Fdevices%2Fe8fdd7df-2ca2-4b64-95bc-031c6b199299&sig=3LOLoFZJcfM6a5EbH7Qhzg0LLu8PMorxjjSibTwvdgs%3D&se=4487176329
+                                                                                                                    ^-- SAS token part -------------------------------------------------------------------------------------------------------------------------------------------------------------^
+LQCloud IoTHub: "iothub-a-prod-loouq.azure-devices.net"
+Note: Leave APPLKEY as an empty string if not using
 */
 
-// devl: "iothub-dev-pelogical.azure-devices.net"
-// prod: "iothub-a-prod-loouq.azure-devices.net"
-
-// #define DEVICESHORTNAME "CloudTstD"
-// #define LQCLOUD_URL "iothub-dev-pelogical.azure-devices.net"
-// #define LQCLOUD_SASTOKEN ""
-// #define LQCLOUD_APPLKEY "notDefined"
-
+// Change the shortname and SAS token to your values to get started. Note:
+// Note: DEVICESHORTNAME is currently a local build only value. LQCloud auto-provisioning will supply from LQCloud.Manage device settings in early 2021 using your device 
+// network hardware (IMEI, MACaddress, etc.) and (organizational) APPLKEY as your device identifiers.
 
 #define DEVICESHORTNAME "CloudTstP"
 #define LQCLOUD_URL "iothub-a-prod-loouq.azure-devices.net"
 #define LQCLOUD_SASTOKEN "SharedAccessSignature sr=iothub-a-prod-loouq.azure-devices.net%2Fdevices%2F867198053226845&sig=%2F58Qbuld6EX5PCJkidWVuKeq75vrjZGKbmmsa00uhQ8%3D&se=1611768392"
-#define LQCLOUD_APPLKEY "notDefined"
-
+#define LQCLOUD_APPLKEY ""
 
 #endif  /* !__DVCSETTINS_H__ */
